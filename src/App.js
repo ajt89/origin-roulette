094129160048efa59107696de3aba278a556a77e
AJ Tuquero <ajtuquero@gmail.com>
2019-10-13 18:08:28 -0700

    Use css to manipulate image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import React, { Component } from 'react';
-import Games from './components/games';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Games games={this.state.games} />
+      <div>
+        {this.state.games.map((game, i) => (
+          <div className='origin-games' key={i}>
+            <div className='game-body'>
+              <h1 className='game-title'>{game.gameName}</h1>
+              <a href={`https://www.origin.com/usa/en-us/store ${game.path}`}><img src={game.image} className='game-image' alt={`${game.gameName} cover art`}></img></a>
+            </div>
+          </div>
+        ))}
+      </div>
     )
   }
 
@@ -15,13 +24,20 @@ class App extends Component {
 
   componentDidMount() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    let originUrl = 'https://api1.origin.com/xsearch/store/en_us/usa/products?searchTerm=&filterQuery=platform:pc-download,gameType:basegame&facetField=subscriptionGroup,genre,gameType,availability,rating,players,language,platform,franchise,publisher,developer,price&sort=&start=0&rows=1&isGDP=true';
+    let searchTerm = '';
+    let filterQuery = 'platform:pc-download,gameType:basegame';
+    let facetField = 'subscriptionGroup,genre,gameType,availability,rating,players,language,platform,franchise,publisher,developer,price';
+    let sort = '';
+    let start = 0;
+    let rows = 1;
+    let isGDP = true
+    let originUrl = `https://api1.origin.com/xsearch/store/en_us/usa/products?searchTerm=${searchTerm}&filterQuery=${filterQuery}&facetField=${facetField}&sort${sort}=&start=${start}&rows=${rows}&isGDP=${isGDP}`;
     fetch(proxyUrl + originUrl)
       .then(res => res.json())
       .then((data) => {
         let totalGames = data.games.numFound;
-        let start = Math.floor(Math.random() * totalGames);
-        let originUrl = 'https://api1.origin.com/xsearch/store/en_us/usa/products?searchTerm=&filterQuery=platform:pc-download,gameType:basegame&facetField=subscriptionGroup,genre,gameType,availability,rating,players,language,platform,franchise,publisher,developer,price&sort=&start=' + start + '&rows=1&isGDP=true';
+        start = Math.floor(Math.random() * totalGames);
+        originUrl = `https://api1.origin.com/xsearch/store/en_us/usa/products?searchTerm=${searchTerm}&filterQuery=${filterQuery}&facetField=${facetField}&sort${sort}=&start=${start}&rows=${rows}&isGDP=${isGDP}`;
         fetch(proxyUrl + originUrl)
           .then(res => res.json())
           .then((data) => {
