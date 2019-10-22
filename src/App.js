41b5566f2b7c8ed5e919f1f0e62cdc1442327102
AJ Tuquero <ajtuquero@gmail.com>
2019-10-21 22:07:22 -0700

    Handle no games condition

--- src/App.js
+++ src/App.js
@@ -5,40 +5,39 @@ import OriginGames from './origin/OriginGames.js'
 import './App.css'
 
 const gameGenres = [
-  {value: 'action', label: 'Action'},
-  {value: 'adventure', label: 'Adventure'},
-  {value: 'arcade', label: 'Arcade'},
-  {value: 'casual', label: 'Casual'},
-  {value: 'family', label: 'Family'},
-  {value: 'first-person', label: 'First Person'},
-  {value: 'horror', label: 'Horror'},
-  {value: 'indie', label: 'Indie'},
-  {value: 'mmo', label: 'MMO'},
-  {value: 'platformer', label: 'Platformer'},
-  {value: 'role-playing', label: 'Role Playing'},
-  {value: 'sci-fi', label: 'Sci-Fi'},
-  {value: 'shooter', label: 'Shooter'},
-  {value: 'simulation', label: 'Simulation'},
-  {value: 'sports', label: 'Sports'},
-  {value: 'strategy', label: 'Strategy'},
-  {value: 'third-person', label: 'Third Person'},
+  { value: 'any', label: 'Choose Your Genre' },
+  { value: 'action', label: 'Action' },
+  { value: 'adventure', label: 'Adventure' },
+  { value: 'arcade', label: 'Arcade' },
+  { value: 'casual', label: 'Casual' },
+  { value: 'family', label: 'Family' },
+  { value: 'first-person', label: 'First Person' },
+  { value: 'horror', label: 'Horror' },
+  { value: 'indie', label: 'Indie' },
+  { value: 'mmo', label: 'MMO' },
+  { value: 'platformer', label: 'Platformer' },
+  { value: 'role-playing', label: 'Role Playing' },
+  { value: 'sci-fi', label: 'Sci-Fi' },
+  { value: 'shooter', label: 'Shooter' },
+  { value: 'simulation', label: 'Simulation' },
+  { value: 'sports', label: 'Sports' },
+  { value: 'strategy', label: 'Strategy' },
+  { value: 'third-person', label: 'Third Person' },
 ]
 
 class App extends Component {
   state = {
-    selectedOption: null,
+    selectedOption: { value: 'any', label: 'Choose Your Genre' },
     refreshWheel: null,
   }
 
   handleChange = selectedOption => {
-    this.setState(
-      { selectedOption },
-      () => console.log(`Option selected:`, this.state.selectedOption)
-    )
+    this.setState({ selectedOption })
   }
 
-  spinTheWheel = () =>
+  spinTheWheel = () => {
     this.setState({ refreshWheel: !this.state.refreshWheel })
+  }
 
   render() {
     const { selectedOption } = this.state;
@@ -53,9 +52,9 @@ class App extends Component {
             <button className='wheel-button' onClick={this.spinTheWheel}>Spin the Wheel!</button>
             <br></br>
             <Select
+              options={gameGenres}
               value={selectedOption}
               onChange={this.handleChange}
-              options={gameGenres}
             />
           </div>
           <br></br>
