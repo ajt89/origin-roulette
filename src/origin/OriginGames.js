ac50ef7fedc3d81cd8bb1574d0e491f797d6fcd8
AJ Tuquero <ajtuquero@gmail.com>
2019-10-21 22:07:22 -0700

    Handle no games condition

--- src/origin/OriginGames.js
+++ src/origin/OriginGames.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import queryStore from './API.js';
 import './OriginGames.css';
+import sadFace from './sad_face_emoji.png';
 
 class OriginGames extends Component {
     state = {
@@ -25,7 +26,7 @@ class OriginGames extends Component {
 
     getRandomGame() {
         let genreQuery = '';
-        if (this.props.refresh.selectedOption) {
+        if (this.props.refresh.selectedOption && this.props.refresh.selectedOption.value !== 'any') {
             genreQuery = `,genre:${this.props.refresh.selectedOption.value}`;
         }
         let searchTerm = '';
@@ -35,11 +36,9 @@ class OriginGames extends Component {
         let start = 0;
         let rows = 1;
         let isGDP = true
-        console.log(filterQuery);
         queryStore(searchTerm, filterQuery, facetField, sort, start, rows, isGDP)
             .then(res => res.json())
             .then((data) => {
-                console.log(data);
                 let totalGames = data.games.numFound;
                 start = Math.floor(Math.random() * totalGames);
                 queryStore(searchTerm, filterQuery, facetField, sort, start, rows, isGDP)
@@ -52,18 +51,32 @@ class OriginGames extends Component {
     }
 
     render() {
+        if (this.state.games) {
+            return (
+                <div className='origin-games'>
+                    {this.state.games.map((game, i) => (
+                        <div className='origin-game' key={i}>
+                            <div className='game-body'>
+                                <br></br>
+                                <h2 className='game-title'>{game.gameName}</h2>
+                                <a href={`https://www.origin.com/usa/en-us/store${game.path}`}><img className='game-cover' src={game.image} alt={`${game.gameName} cover art`}></img></a>
+                                <br></br>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )
+        }
         return (
             <div className='origin-games'>
-                {this.state.games.map((game, i) => (
-                    <div className='origin-game' key={i}>
-                        <div className='game-body'>
-                            <br></br>
-                            <h2 className='game-title'>{game.gameName}</h2>
-                            <a href={`https://www.origin.com/usa/en-us/store${game.path}`}><img className='game-cover' src={game.image} alt={`${game.gameName} cover art`}></img></a>
-                            <br></br>
-                        </div>
+                <div className='origin-game'>
+                    <div className='game-body'>
+                        <br></br>
+                        <h2 className='game-title'>No Games Found</h2>
+                        <img className='game-error' src={sadFace} alt={'Sad Face'}></img>
+                        <br></br>
                     </div>
-                ))}
+                </div>
             </div>
         )
     }
